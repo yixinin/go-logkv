258a9d44d8d253339d24ba21112501538ec20b98
yixin <ranyixin@xiaoduotech.com>
2021-06-30 20:03:17 +0800

    up

--- kv/engine.go
+++ kv/engine.go
@@ -15,41 +15,49 @@ type EngineMeta struct {
 
 type KvEngine struct {
 	sync.Mutex
-	meta EngineMeta
-	Fd   *os.File
+	meta    EngineMeta
+	fd      *os.File
+	indexer *KvIndexer
 }
 
 func NewKvEngine(filename string) *KvEngine {
-	return &KvEngine{
+	e := &KvEngine{
 		meta: EngineMeta{
 			filename: filename,
 		},
+		indexer: NewKvIndexer(),
 	}
+	var err error
+	e.fd, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModeAppend|os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	return e
 }
 
 func (e *KvEngine) Set(kv protocol.Kv) error {
 	e.Lock()
 	defer e.Unlock()
-	_, err := e.Fd.Write(kv.Bytes())
+	_, err := e.fd.Write(kv.Bytes())
 	return err
 }
 
 func (e *KvEngine) RawSet(data []byte) error {
 	e.Lock()
 	defer e.Unlock()
-	_, err := e.Fd.Write(data)
+	_, err := e.fd.Write(data)
 	return err
 }
 
 func (e *KvEngine) rawDataSet(data []byte) error {
-	_, err := e.Fd.Write(data)
+	_, err := e.fd.Write(data)
 	return err
 }
 
 func (e *KvEngine) BatchSet(kvs protocol.Kvs) error {
 	e.Lock()
 	defer e.Unlock()
-	_, err := e.Fd.Write(kvs.Bytes())
+	_, err := e.fd.Write(kvs.Bytes())
 	return err
 }
 
--- kv/index.go
+++ kv/index.go
@@ -1,1 +1,95 @@
 package kv
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+type KvIndexer struct {
+	i map[string]Index
+}
+
+func NewKvIndexer() *KvIndexer {
+	return &KvIndexer{
+		i: make(map[string]Index, 1),
+	}
+}
+
+type IndexKey struct {
+	stringVal *string
+	intVal    *int
+	floatVal  *float64
+}
+
+func NewIndexKey(val interface{}) IndexKey {
+	var stringVal string
+	var intVal int
+	var floatVal float64
+	switch v := val.(type) {
+	case string:
+		stringVal = v
+	case int:
+		intVal = v
+	case uint:
+		intVal = int(v)
+	case int32:
+		intVal = int(v)
+	case uint32:
+		intVal = int(v)
+	case int64:
+		intVal = int(v)
+	case uint64:
+		intVal = int(v)
+	case float64:
+		floatVal = v
+	case float32:
+		floatVal = float64(v)
+	default:
+		switch reflect.ValueOf(val).Kind() {
+		case reflect.String:
+			stringVal = fmt.Sprintf("%s", val)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			intVal, _ = strconv.Atoi(fmt.Sprintf("%d", val))
+		case reflect.Float32:
+			floatVal, _ = strconv.ParseFloat(fmt.Sprintf("%f", val), 32)
+		case reflect.Float64:
+			floatVal, _ = strconv.ParseFloat(fmt.Sprintf("%f", val), 64)
+		}
+	}
+	var k = IndexKey{}
+	if stringVal != "" {
+		k.stringVal = &stringVal
+	}
+	if intVal != 0 {
+		k.intVal = &intVal
+	}
+	if floatVal != 0 {
+		k.floatVal = &floatVal
+	}
+	return k
+}
+
+func (k IndexKey) String() string {
+	if k.stringVal != nil {
+		return *k.stringVal
+	}
+	return ""
+}
+func (k IndexKey) Int() int {
+	if k.stringVal != nil {
+		return *k.intVal
+	}
+	return 0
+}
+func (k IndexKey) Float() float64 {
+	if k.floatVal != nil {
+		return *k.floatVal
+	}
+	return 0
+}
+
+type Index struct {
+	i map[IndexKey]uint64
+}
